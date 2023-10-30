646f12712bdbb2e9680c4d6db8452f756e5f64ca
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2023-10-29 22:05:37 -0400

    remove inline test program
    was using this to directly test the machine code

--- src/emulator/main.c
+++ src/emulator/main.c
@@ -11,41 +11,7 @@ int main(int argc, char const* argv[]) {
     reset_registers(&registers);
     mem_init(memory);
 
-    // mov @a 1
-    memory[0] = MOV_IMM;
-    memory[1] = 0;
-    memory[2] = 1;
-
-    // mov @b 1
-    memory[3] = MOV_IMM;
-    memory[4] = 1;
-    memory[5] = 2;
-
-    // sub @a @b
-    memory[6] = SUB_REG;
-    memory[7] = 0;
-    memory[8] = 1;
-
-    // jn 0xf
-    memory[9] = JN;
-    memory[10] = 0x00;
-    memory[11] = 0xf;
-
-    // add @a 1
-    memory[12] = ADD_IMM;
-    memory[13] = 0;
-    memory[14] = 1;
-
-    // add @a 2
-    memory[15] = ADD_IMM;
-    memory[16] = 0;
-    memory[17] = 2;
-
-    memory[18] = HLT;
-
     cpu_run_program(&registers, memory);
-
-    printf("0x%x\n", registers.general_purpose.a);
 
     return 0;
 }
