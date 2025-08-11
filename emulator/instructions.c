e32afccca053a28a11dedba0e80f9b687542e2e5
kryllyxofficial01 <kryllyxofficial@gmail.com>
2025-08-11 10:48:46 -0400

    implement `lda` and `stb`

--- emulator/instructions.c
+++ emulator/instructions.c
@@ -38,11 +38,45 @@ void trk8_mov(trk8_memory_t* memory, trk8_registers_t* registers, uint8_t operan
 }
 
 void trk8_lda(trk8_memory_t* memory, trk8_registers_t* registers, uint8_t operands_type) {
+    memory_increment_program_counter(memory, 1);
 
+    registers->address.low = memory_fetch_byte(*memory, memory_get_program_counter(*memory));
+
+    memory_increment_program_counter(memory, 1);
+
+    registers->address.high = memory_fetch_byte(*memory, memory_get_program_counter(*memory));
+
+    memory_increment_program_counter(memory, 1);
 }
 
 void trk8_stb(trk8_memory_t* memory, trk8_registers_t* registers, uint8_t operands_type) {
+    uint16_t address = TRK8_WORD(
+        registers->address.high,
+        registers->address.low
+    );
 
+    memory_increment_program_counter(memory, 1);
+
+    switch (operands_type) {
+        case TRK8_OPERANDS_TYPE_IMM8: {
+            uint8_t data = memory_fetch_byte(*memory, memory_get_program_counter(*memory));
+
+            memory_write_byte(memory, address, data);
+
+            break;
+        }
+
+        case TRK8_OPERANDS_TYPE_REGISTER: {
+            uint8_t register_id = memory_fetch_byte(*memory, memory_get_program_counter(*memory));
+            uint8_t register_data = registers_get(*registers, register_id);
+
+            memory_write_byte(memory, address, register_data);
+
+            break;
+        }
+    }
+
+    memory_increment_program_counter(memory, 1);
 }
 
 void trk8_ldb(trk8_memory_t* memory, trk8_registers_t* registers, uint8_t operands_type) {
--- emulator/main.c
+++ emulator/main.c
@@ -1,15 +1,17 @@
 #include "include/cpu.h"
 
 int main(void) {
-    uint8_t program[6] = { 0x14, 0x00, 0x19, 0x16, 0x01, 0x00 };
+    uint8_t program[8] = { 0x14, 0x00, 0x19, 0x1a, 0x00, 0xff, 0x23, 0x00 };
 
     trk8_memory_t memory;
     trk8_registers_t registers;
 
-    memory_init(&memory, program, 6);
+    memory_init(&memory, program, 8);
     registers_init(&registers);
 
     cpu_run(&memory, &registers);
 
     registers_debug_print(registers);
+
+    printf("0x%x\n", memory_fetch_byte(memory, 0xff00));
 }
