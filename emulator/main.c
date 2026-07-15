5acec934545d93815e95f0b500493c3b306c9450
kryllyxofficial01 <kryllyxofficial@gmail.com>
2026-07-15 09:36:17 -0400

    create main file + setup base memory struct

--- emulator/main.c
+++ emulator/main.c
@@ -1,18 +1,5 @@
-#include "include/cpu.h"
+#include "include/memory.h"
 
 int main(void) {
-    #define PROGRAM_LENGTH (6)
-
-    uint8_t program[PROGRAM_LENGTH] = {
-        0x14, 0x00, 0xff, // mov %a, 0xff
-        0x44, 0x00, 0x01  // adc %a, 0x01 (should enable the carry flag)
-    };
-
-    trk8_machine_t machine;
-
-    trk8_init(&machine);
-
-    memory_write_program(machine.memory, program, PROGRAM_LENGTH);
-
-    cpu_run(&machine);
+    trk8_memory_t memory = memory_init();
 }
